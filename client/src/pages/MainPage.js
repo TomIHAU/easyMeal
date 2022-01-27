2680090f2c256760e940f7aff176461fe0587d2e
TauH <thaywood988@gmail.com>
2022-01-27 19:36:12 +1100

    main page layout done

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,7 +3,34 @@ import React from "react";
 export default function MainPage() {
   return (
     <div className="App">
-      <h1>this is the app</h1>
+      <div className="mainBanner">
+        <div className="mainBannerInfo">
+          <h1>This is great</h1>
+          <p>
+            lorem ipsum asldkjfgasdlfhlksdj aflaskjdf lskadfjh alsdkf
+            jasdlfkjhasd flkasjdfh laskdfj h
+          </p>
+          <button>Order Now</button>
+        </div>
+        <div className="mainImgContainer">
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+        </div>
+      </div>
+
+      <div className="firstSales">
+        <h2>head for selling the product</h2>
+        <p>
+          some sales stuff lorem ipsum asldkfjahslkdfjhsadlkfhalssd dsfa fdas
+          fasf asf
+        </p>
+        <div className="firstSalesLinks">
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+          <img src="./img/foodTable.jpg" alt="food pic to make you hungry" />
+        </div>
+      </div>
     </div>
   );
 }
