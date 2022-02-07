b04d7fa838030541e44c125ec747f1813cb6f40c
TauH <thaywood988@gmail.com>
2022-02-07 20:53:14 +1100

    changed to table on enchance

--- client/src/components/MoreDetailsMeal.js
+++ client/src/components/MoreDetailsMeal.js
@@ -42,9 +42,32 @@ export default function MoreDetailsMeal({ mealId, setMoreDetails, addToCart }) {
                 </p>
               </div>
               <div>
-                <p>Fat: {enhance.fat}</p>
-                <p>protein:{enhance.protein}</p>
-                <p>Carbs:{enhance.carbs}</p>
+                <table>
+                  <tr>
+                    <td>Fat, Total</td>
+                    <td>{enhance.fat}</td>
+                  </tr>
+                  <tr>
+                    <td>Fat, Saturated</td>
+                    <td>{enhance.fat - 2}</td>
+                  </tr>
+                  <tr>
+                    <td>Carbs</td>
+                    <td>{enhance.carbs}</td>
+                  </tr>
+                  <tr>
+                    <td>Sugars</td>
+                    <td>{enhance.carbs - 1}</td>
+                  </tr>
+                  <tr>
+                    <td>Protein</td>
+                    <td>{enhance.protein}</td>
+                  </tr>
+                  <tr>
+                    <td>Sodium</td>
+                    <td>{Math.floor(Math.random() * 1000 + 1)}mg</td>
+                  </tr>
+                </table>
               </div>
             </div>
           </div>
